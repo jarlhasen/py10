e51d23b11cc6c63c5f27c1a2c49787779195e735
Yaakov Azat <yaakovazat@gmail.com>
2018-07-06 18:13:05 +0600

    We have made the programm to suggest a correct word when the user have a wrong input as ,did you mean %s instead?

--- translator/app.py
+++ translator/app.py
@@ -4,19 +4,26 @@ from difflib import get_close_matches #we have to consider that user may enter a
 
 data = json.load(open("data.json"))
 
+# def translate(w):
+#     w = w.lower() #Every word there is a lover case letters in the data file ,so we can turn every letter to a lower case letters first.
+#     if w in data:
+#         return data[w]
+#     else:
+#         return "The word dosen't exist ,please double check it !"
+
+# version two : we make the program alittle bit smarter let's make really interesting
+
 def translate(w):
-    w = w.lower() #Every word there is a lover case letters in the data file ,so we can turn every letter to a lower case letters first.
+    w = w.lower()
     if w in data:
         return data[w]
-    else:
-        return "The word dosen't exist ,please double check it !"
-
+    elif len(get_close_matches(w,data.keys())) >0:
+        return "Did you mean %s instead?" % get_close_matches(w,data.keys())[0]
 word = input("Enter a word:")
-
 print(translate(word))
 
 
-#
 # remember to use python standard library
 
 # we also have to match out the most similar words given by the user and return a most possible words by.
+
